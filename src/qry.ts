38b43a839e0561d7b1ea917ecb7c59a6762c316c
Kevin Kondratuk <paintface07@gmail.com>
2024-05-04 10:36:50 -0500

    init

---
+++ src/qry.ts
@@ -0,0 +1,55 @@
+
+export default (context: Context, args?: unknown) => {
+    let {caller} = context
+    const l = $fs.scripts.lib()
+
+    const cmd = args["command"]
+    const query = args["query"]
+    const data = args["data"]
+
+    if(cmd === null || typeof(cmd) !== "string") {
+        return {ok: false, err: "command must be specified and must be a string"}
+    }
+
+    if((cmd === "select" || cmd === "delete")
+        && (query === null || typeof(query) !== "object")) {
+        return {ok: false, err: "query must be specified and must be an object"}
+    }
+
+    if((cmd === "delete")
+        && !(data !== null && (!isArrayofObjects(data) || typeof data !== "object"))) {
+        return {ok: false, err: "data must be a Array"}
+    }
+
+    switch(cmd) {
+        case "select":
+            const c = $db.f(args["query"])
+            c.each((doc) => $D(doc))
+            c.close()
+            break
+        case "insert":
+            $D(data)
+            const ir = $db.i(data)
+            if(!ir.ok) {
+                return ir
+            }
+            break
+        case "delete":
+            const dr = $db.r(query)
+            if(!dr.ok) {
+                return dr
+            }
+            break
+        default:
+            return {ok: false, err: "invalid command: valid commands are select, insert, or delete"}
+    }
+}
+
+function isArrayofObjects(value: any): value is object[] {
+    // Check if the value is an array
+    if (!Array.isArray(value)) {
+        return false;
+    }
+    // Check if every element in the array is an object
+    return value.every(item => typeof item === 'object' && item !== null);
+}
